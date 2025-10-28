207ef5f56adbe95034a18e26121d89d808ac9ef3
cobr31 <cobr31@live.nl>
2025-10-28 23:42:30 +0100

    userGetFunction

--- index.js
+++ index.js
@@ -25,7 +25,80 @@ const userGetFunction = (name) => {
     });
 }
 
+const repoGetFunction = (name) => {
+    axios( api + name + "/repos?sort=created" )
+    .then((response) => {
+        repoCardFunction(response.data);
+    })
+    .catch((err) => {
+        errorFunction(
+            "Problem occured fetching repository"
+        );
+    });
+}
+const userCard = (user) => {
+    let id = user.name || user.login;
+    let info = user.bio
+        ? `<p>${user.bio}</p>`: "";
+    let cardElement = `
+<div class="card">
+    <div>
+    <img src="${user.avatar_url}
+        alt="${user.name}"
+        class="avatar">
+    </div>
+
+    <div class="user-info">
+        <h2>${id}</h2>${info}
+        <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+        <div id="repos"></div>
+    </div>
+</div>`;
+    main.innerHTML = cardElement
+}
+
+const errorFunction = (repos) => {
+    let cardHTML = `
+    <div class="card">
+    <h1>${error}</h1>
+    </div>`;
+        main.innerHTML = cardHTML
+}
+
+const repoCardFunction = (repos) => {
+    let reposElement =
+     document.getElementById("repos");
+    for (let i = 0; i < 5 && i < 5 && i < repos.length; i++) {
+        let repo = repos[i];
+        let repoEl = document.createElement("a");
+        repoEl.classList.add("repo");
+        repoEl.href = repo.html_url;
+        repoEl.target = "_blank";
+        repoEl.innerText = repo.name;
+        reposElement.appendChild(repoEl);
+    }}
+    inputForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let user = inputBox.ariaValueMax;
+        if (user) {
+            userGetFunction(user);
+            inputBox.value = "";
+        }
+});
 
 
 
-`
+
+
+
+
+
+
+
+
+
+
